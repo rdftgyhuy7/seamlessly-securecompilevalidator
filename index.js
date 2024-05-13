3cf02805ac69e02bbfb56d4a903eb47426ee6ec9
rdftgyhuy7 <gs1jean5467@hotmail.com>
2024-05-13 08:55:38 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-function generateParenthesis(n) {
-  const result = [];
-  backtrack("", 0, 0);
-  return result;
-  function backtrack(current, open, close) {
-    if (current.length === 2 * n) result.push(current);
-    if (open < n) backtrack(current + "(", open + 1, close);
-    if (close < open) backtrack(current + ")", open, close + 1);
-  }
+function sayHi() {
+  console.log("Hi!");
 }
+
+setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
